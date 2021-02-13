c992212b2f7679b322990a9ea409c751126b6d8c
haru1843 <haru1843inf@gmail.com>
2021-02-13 16:34:27 +0900

    add: slidev1 at home

---
+++ client/src/views/homeComponents/slide_v1.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const StyledDiv = styled.div``;
+
+function SlideV1(props) {
+  return (
+    <StyledDiv>
+      <div>
+        <img></img>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </StyledDiv>
+  );
+}
